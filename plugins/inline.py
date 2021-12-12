0b8a1782300f2d8e290a255b13f336a198a31a43
GitHub User <88586718+aanzal@users.noreply.github.com>
2021-12-12 14:41:06 +0530

    -

--- plugins/inline.py
+++ plugins/inline.py
@@ -17,7 +17,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='You have to Subscribe our Channel to Use me.',
                            switch_pm_parameter="subscribe")
         return
 
@@ -91,7 +91,10 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('Developers', url="https://t.me/ZaynAndMillie")
+        ],
+        [
+            InlineKeyboardButton('Search 🔍', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
