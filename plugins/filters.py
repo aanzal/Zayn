a18760efce0884cfa984440205adc7d5a02d7981
GitHub User <88586718+aanzal@users.noreply.github.com>
2021-12-12 14:29:34 +0530

    -

--- plugins/filters.py
+++ plugins/filters.py
@@ -159,7 +159,7 @@ async def get_all(client, message):
         filterlist = f"Total number of filters in **{title}** : {count}\n\n"
 
         for text in texts:
-            keywords = " ×  `{}`\n".format(text)
+            keywords = " ⇢  `{}`\n".format(text)
 
             filterlist += keywords
 
@@ -172,7 +172,7 @@ async def get_all(client, message):
                 )
             return
     else:
-        filterlist = f"There are no active filters in **{title}**"
+        filterlist = f"Active filters in **{title}**\nFilters : 0"
 
     await message.reply_text(
         text=filterlist,
@@ -180,11 +180,11 @@ async def get_all(client, message):
         parse_mode="md"
     )
         
-@Client.on_message(filters.command('del') & filters.incoming)
+@Client.on_message(filters.command(['del', 'delf']) & filters.incoming)
 async def deletefilter(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
-        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
+        return await message.reply(f"You are Anonymous Admin. Use /cchat {message.chat.id} in PM")
     chat_type = message.chat.type
 
     if chat_type == "private":
@@ -195,10 +195,10 @@ async def deletefilter(client, message):
                 chat = await client.get_chat(grpid)
                 title = chat.title
             except:
-                await message.reply_text("Make sure I'm present in your group!!", quote=True)
+                await message.reply_text("**Make sure I'm present in your group!**\n\nRun /myconnections to Know Connected Groups.", quote=True)
                 return
         else:
-            await message.reply_text("I'm not connected to any groups!", quote=True)
+            await message.reply_text("**I'm not connected to any groups!**\n\nRun /myconnections to Know Connected Groups.", quote=True)
 
     elif chat_type in ["group", "supergroup"]:
         grp_id = message.chat.id
@@ -220,7 +220,7 @@ async def deletefilter(client, message):
     except:
         await message.reply_text(
             "<i>Mention the filtername which you wanna delete!</i>\n\n"
-            "<code>/del filtername</code>\n\n"
+            "<code>/delf [filtername]</code>\n\n"
             "Use /viewfilters to view all available filters",
             quote=True
         )
@@ -231,11 +231,11 @@ async def deletefilter(client, message):
     await delete_filter(message, query, grp_id)
         
 
-@Client.on_message(filters.command('delall') & filters.incoming)
+@Client.on_message(filters.command('delallf') & filters.incoming)
 async def delallconfirm(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
-        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
+        return await message.reply(f"You are Anonymous Admin. Use /cchat {message.chat.id} in PM")
     chat_type = message.chat.type
 
     if chat_type == "private":
@@ -246,10 +246,10 @@ async def delallconfirm(client, message):
                 chat = await client.get_chat(grpid)
                 title = chat.title
             except:
-                await message.reply_text("Make sure I'm present in your group!!", quote=True)
+                await message.reply_text("**Make sure I'm present in your group!**\n\nRun /myconnections to Know Connected Groups.", quote=True)
                 return
         else:
-            await message.reply_text("I'm not connected to any groups!", quote=True)
+            await message.reply_text("**I'm not connected to any groups!**\n\nRun /myconnections to Know Connected Groups.", quote=True)
             return
 
     elif chat_type in ["group", "supergroup"]:
